c0b48e23fa03343d479ed9ec131ef0086b125fdb
Matarawy <mahmoudsarhan02672@gmail.com>
2023-05-15 14:39:35 +0300

    initial files

---
+++ Obstacal avoiding car/Obstacal avoiding car/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\button\button.c
+
+HAL\keypad\keypad and LCD intergration.c
+
+HAL\keypad\keypad.c
+
+HAL\lcd\lcd.c
+
+HAL\lcd\lcd_test.c
+
+HAL\motor\motor.c
+
+HAL\timer_manager\timer_manager.c
+
+main.c
+
+MCAL\dio\dio.c
+
+MCAL\timer\timer.c
+
---
+++ Obstacal avoiding car/Obstacal avoiding car/main.c
@@ -0,0 +1,18 @@
+/*
+ * Obstacal avoiding car.c
+ *
+ * Created: 5/15/2023 2:33:27 PM
+ * Author : engma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
